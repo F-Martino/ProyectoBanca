733e6f3d37827ad1f1d97b67539f276ae8ba3d2e
fernando.martino.garcia <fernando.martino.97@gmail.com>
2020-05-13 13:13:18 +0200

    Creada clase Tarjeta

---
+++ src/Tarjeta.java
@@ -0,0 +1,46 @@
+
+public class Tarjeta {
+	private String numeroTarjeta;
+	private double limite;
+	private int numeroUsos;
+		
+	public Tarjeta(String numeroTarjeta, double limite, int numeroUsos) {
+		this.numeroTarjeta = numeroTarjeta;
+		this.limite = limite;
+		this.numeroUsos = numeroUsos;
+	}
+	
+	public Tarjeta(String numeroTarjeta, double limite) {
+		this.numeroTarjeta = numeroTarjeta;
+		this.limite = limite;
+		this.numeroUsos = 0;
+	}
+
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+	public double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+
+	public int getNumeroUsos() {
+		return numeroUsos;
+	}
+
+	public void setNumeroUsos(int numeroUsos) {
+		this.numeroUsos = numeroUsos;
+	}
+	
+	
+	
+	
+}
