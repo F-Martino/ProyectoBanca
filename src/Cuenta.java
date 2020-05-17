1c4b14e972db8de4af0da9bcdae534fb06553ee8
fernando.martino.garcia <fernando.martino.97@gmail.com>
2020-05-17 14:02:08 +0200

    Cambios para el saldo medio

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 /**
  * Clase que guarda los datos de una cuenta.
- * El número de cuenta, saldo, límite de retirada, fecha de apertura y tarjetas asociadas a la cuenta.
+ * El número de cuenta, saldo, límite de retirada, fecha de apertura, la lista del saldo al comienzo de mes durante los últimos 12 meses y tarjetas asociadas a la cuenta.
  * @author Fernando Martino
  *
  */
@@ -11,39 +11,45 @@ public class Cuenta {
 	private double saldo;
 	private double limite;
 	private String fechaApertura;
+	private ArrayList<Double> saldoDoceMeses;
 	private ArrayList<Tarjeta> tarjetas;
+	
 	
 	/**
 	 * Constructor de la clase Cuenta, recibiendo como parametros el número de cuenta,
-	 * el saldo, el límite de retirada y la fecha de apertura.
+	 * el saldo, el límite de retirada, la fecha de apertura y la lista del saldo mensual.
 	 * Inicializa la lista de tarjetas como una lista vacía.
 	 * @param numeroCuenta String - Numero de cuenta.
 	 * @param saldo double - Saldo de la cuenta.
 	 * @param limite double - Limite de retirada permitido en la cuenta.
 	 * @param fechaApertura String - Día en que la cuenta fue creada.
+	 * @param saldoDoceMeses ArrayList[Double] - Lista del saldo mensual los últimos 12 meses.
 	 */
-	public Cuenta(String numeroCuenta, double saldo, double limite, String fechaApertura) {
+	public Cuenta(String numeroCuenta, double saldo, double limite, String fechaApertura, ArrayList<Double> saldoDoceMeses) {
 		this.numeroCuenta = numeroCuenta;
 		this.saldo = saldo;
 		this.limite = limite;
 		this.fechaApertura = fechaApertura;
+		this.saldoDoceMeses = saldoDoceMeses;
 		this.tarjetas = new ArrayList<>();
 	}
 	
 	/**
 	 * Constructor de la clase Cuenta, recibiendo como parametros el número de cuenta,
-	 * el saldo, el límite de retirada, la fecha de apertura y la lista de las tarjetas.
+	 * el saldo, el límite de retirada, la fecha de apertura, la lista del saldo mensual y la lista de las tarjetas.
 	 * @param numeroCuenta String - Número de cuenta.
 	 * @param saldo double - Saldo de la cuenta.
 	 * @param limite double - Límite de retirada permitido en la cuenta.
 	 * @param fechaApertura String - Día en que la cuenta fue creada.
+	 * @param saldoDoceMeses ArrayList[Double] - Lista del saldo mensual los últimos 12 meses.
 	 * @param tarjetas ArrayList[Tarjetas] - Lista con las tarjetas asociadas a la cuenta.
 	 */
-	public Cuenta(String numeroCuenta, double saldo, double limite, String fechaApertura, ArrayList<Tarjeta> tarjetas) {
+	public Cuenta(String numeroCuenta, double saldo, double limite, String fechaApertura, ArrayList<Double> saldoDoceMeses, ArrayList<Tarjeta> tarjetas) {
 		this.numeroCuenta = numeroCuenta;
 		this.saldo = saldo;
 		this.limite = limite;
 		this.fechaApertura = fechaApertura;
+		this.saldoDoceMeses = saldoDoceMeses;
 		this.tarjetas = tarjetas;
 	}
 
@@ -110,7 +116,50 @@ public class Cuenta {
 	public void setFechaApertura(String fechaApertura) {
 		this.fechaApertura = fechaApertura;
 	}
+	
+	/**
+	 * Método que devuelve la lista con los saldos de la cuenta al comienzo de cada mes durante los últimos 12 meses.
+	 * @return saldoDoceMeses ArrayList[Double] - Lista del saldo mensual los últimos 12 meses.
+	 */
+	public ArrayList<Double> getSaldoDoceMeses() {
+		return saldoDoceMeses;
+	}
 
+	/**
+	 * Método que permite modificar la lista de los saldos mensuales de la cuenta.
+	 * @param saldoDoceMeses ArrayList[Double] - Nueva lista del saldo mensual.
+	 */
+	public void setSaldoDoceMeses(ArrayList<Double> saldoDoceMeses) {
+		this.saldoDoceMeses = saldoDoceMeses;
+	}
+
+	/**
+	 * Método que al cambiar de mes añade el saldo de la cuenta del nuevo mes,
+	 * si ya había guardados 12 meses borra el más antigüo
+	 * @param saldoNuevoMes
+	 */
+	public void nuevoMes(double saldoNuevoMes) {
+		if(this.saldoDoceMeses.size() < 12)
+			this.saldoDoceMeses.add(saldoNuevoMes);
+		else {
+			this.saldoDoceMeses.remove(0);
+			this.saldoDoceMeses.add(saldoNuevoMes);
+		}
+	}
+	
+	/**
+	 * Método que devuelve el saldo medio de la cuenta los últimos 12 meses.
+	 * @return res double - Saldo medio los últimos 12 meses.
+	 */
+	public double saldoMedio() {
+		double sum = 0;
+		for(double i: this.saldoDoceMeses)
+			sum += i;
+		double res = sum / 12;
+		
+		return res;
+	}
+	
 	/**
 	 * Método que devuelve la lista de tarjetas asociadas a la cuenta.
 	 * @return tarjetas ArrayList[Tarjetas] - Lista de las tarjetas.
